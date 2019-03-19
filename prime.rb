f0f35c106a1922efbc36b08f2d993466d3e4deaf
Will Jones <willjones2010@gmail.com>
2019-03-19 22:53:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ def prime?(num)
  for d in 2..(num-1)
  if num < 1 
   return false
-elsif num <= 3
+ elsif num <= 3
   return true
-   else (num % d) == 0 
-     return false
+ else (num % d) == 0 
+  return false
 end
 end
 end
