f9f0d9fa8ba81c01ae1ca42196dc77c55a8f6694
Will Jones <willjones2010@gmail.com>
2019-03-19 22:55:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
-def prime?(num)
- for d in 2..(num-1)
- if num < 1 
-  return false
- elsif num <= 3
-  return true
- else (num % d) == 0 
-  return false
-end
-end
-end
+# def prime?(num)
+# for d in 2..(num-1)
+# if num < 1 
+#   return false
+# elsif num <= 3
+#   return true
+# else (num % d) == 0 
+#   return false
+# end
+# end
+# end
